500223752beb92b4ffd7fe3c222b625afd9004fe
Borja S <borjas@marc-roig.local>
2019-03-25 18:55:06 +0100

    Initial Commit

---
+++ TowerDefenseTests/TowerDefenseTests.swift
@@ -0,0 +1,34 @@
+//
+//  TowerDefenseTests.swift
+//  TowerDefenseTests
+//
+//  Created by Borja S on 25/03/2019.
+//  Copyright © 2019 Borja S. All rights reserved.
+//
+
+import XCTest
+@testable import TowerDefense
+
+class TowerDefenseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
